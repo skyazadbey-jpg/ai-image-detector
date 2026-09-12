8a657ce1a8979c9aa44ace3f5a7ae6fd2bf716cc
skyazadbey-jpg <skyazadbey@gmail.com>
2026-09-12 15:55:01 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import io
 app = FastAPI()
 
 API_URL = "https://router.huggingface.co/hf-inference/models/umm-maybe/AI-image-detector"
-HEADERS = {"Authorization": "Bearer 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"}
+HEADERS = {"Authorization": "Bearer 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"}
 
 @app.get("/")
 def home():
