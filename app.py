ada891c6324c2edba7669a78296b6702e8479410
skyazadbey-jpg <skyazadbey@gmail.com>
2026-09-12 16:58:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,12 +11,12 @@ HEADERS = {"Authorization": "Bearer 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"}
 def home():
     return {"status": "AI Image Detector API is running (Lightweight Mode)"}
 
-@app.post("/predict")
+@post("/predict")
 async def predict(file: UploadFile = File(...)):
     try:
         contents = await file.read()
         response = requests.post(API_URL, headers=HEADERS, data=contents)
-
+        
         if response.status_code != 200:
             return {"error": f"Hugging Face API Error: {response.text}"}
 
