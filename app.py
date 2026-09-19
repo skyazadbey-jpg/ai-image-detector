2d883fe381d570b695b254e9dfb01b6a518b682d
skyazadbey-jpg <skyazadbey@gmail.com>
2026-09-19 23:06:42 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -476,8 +476,6 @@ async def predict(file: UploadFile = File(None), image_url: str = Form(None)):
         r3 = requests.post(API_URL_3, headers=headers, data=contents, timeout=60)
         scores = []
         for r in [r1, r2, r3]:
-                    # Model 3: Flux dedektörü
-        r3 = requests.post(API_URL_3, headers=headers, data=contents, timeout=60)
             if r.status_code == 200:
                 for item in r.json():
                     if isinstance(item, dict) and item.get("label") in ["Fake", "fake", "ai", "artificial"]:
