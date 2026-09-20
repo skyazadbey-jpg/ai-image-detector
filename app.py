babb01942997e4f2282c139a899c956c91854f04
skyazadbey-jpg <skyazadbey@gmail.com>
2026-09-20 12:13:23 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -28,7 +28,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    ...
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # ---------- CONFIG ----------
